c2b1f9edbe397d3269499d7fc6f020b7f082ab95
Toncho Lozev <toncho.lozev@yahoo.com>
2019-07-31 18:26:40 +0300

    solution for filtering the array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function filterArray(array){
+  let uniqueArr = [];
+  for(let i = 0; i < array.length; i++){
+    const curr = array[i];
+    const next = array[i+1];
+    if(curr !== next){
+      uniqueArr.push(curr);
+    }
+  }
+  return uniqueArr;
+}
+
 const App = () => {
   return (
     <div>
+      {filterArray([1, 2, 2, 2, 3, 4, 4, 4, 4, 4, 4, 5])}
       <p>React here!</p>
     </div>
   );
