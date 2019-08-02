2ace7fbd9f22924178400eb94f3f3c8c4e7340cd
Toncho Lozev <toncho.lozev@yahoo.com>
2019-08-02 15:57:23 +0300

    added remove array and select array functionality

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # react-task
+1.You will need npm
+2.Install packages with command "npm i"
+3.Run "npm start" and then the window will open automatically or open localhost on port 8080 ("localhost:8080")
--- src/App.js
+++ src/App.js
@@ -3,79 +3,107 @@ import React, {PureComponent} from 'react';
 import filterArray from './helpers/filterArray';
 
 class App extends PureComponent {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            input: '',
-            lettersInput: false,
-            history: []
-        };
+    this.state = {
+      input: '',
+      lettersInput: false,
+      history: []
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleFilter = this.handleFilter.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
+    this.deleteArray = this.deleteArray.bind(this);
+    this.selectArray = this.selectArray.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({input: event.target.value});
+  }
+
+  handleFilter() {
+    const {input} = this.state;
+    const regExLetters = /[a-zA-Z]/;
+    const regExNumbers = /\d+/g;
+
+    if (!input) {
+      return;
     }
 
-    handleChange(event) {
-        this.setState({input: event.target.value});
+    if (regExLetters.test(input)) {
+      this.setState({lettersInput: true});
+      return;
     }
 
-    handleFilter() {
-        const {input} = this.state;
-        const regExLetters = /[a-zA-Z]/;
-        const regExNumbers = /\d+/g;
+    const arr = input.match(regExNumbers);
 
-        if (!input) {
-            return;
-        }
+    this.setState({
+      lettersInput: false,
+      history: [...this.state.history, filterArray(arr)]
+    });
+  }
 
-        if (regExLetters.test(input)) {
-            this.setState({lettersInput: true});
-            return;
-        }
+  deleteArray(index) {
+    this.setState({
+      history: this.state.history.filter((el, i) => i !== index)
+    });
+  }
 
-        const arr = input.match(regExNumbers);
+  selectArray(index) {
+    const {history} = this.state;
+    const selected = history[index];
 
-        this.setState({
-            lettersInput: false,
-            history: [...this.state.history, filterArray(arr)]
-        });
-    }
+    this.setState({
+      input: selected.join(', ')
+    });
+  }
 
-    render() {
-        const {lettersInput, history} = this.state;
+  render() {
+    const {lettersInput, history, input} = this.state;
 
-        const historyItems = history.map((el, index) => {
-            return <li key={index} className="list-group-item py-2 px-3">[{el.join(', ')}]</li>;
-        });
+    const historyItems = history.map((el, index) => {
+      return <li key={index} className="list-group-item d-flex align-items-baseline justify-content-between py-2 px-3">
+        <span>[{el.join(', ')}]</span>
+        <div>
+          <i className="fas fa-plus-circle mr-2" onClick={() => this.selectArray(index)}></i>
+          <i className="fas fa-minus-circle" onClick={() => this.deleteArray(index)}></i>
+        </div>
+      </li>;
+    });
 
-        return (
-            <div className="container py-5">
-                <div className="row">
-                    <div className="col-12">
-                        {lettersInput ? <p>Huh... we don't need letters here</p> : ''}
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="input-group col-6 mb-3">
-                        <input type="text" className="form-control" placeholder="Enter the array"
-                               onChange={this.handleChange}/>
-                        <div className="input-group-append">
-                            <button className="btn btn-outline-secondary" type="button"
-                                    onClick={this.handleFilter}>Filter
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-6">
-                        <ul className="list-group">
-                            {historyItems}
-                        </ul>
-                    </div>
-                </div>
+    return (
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-12">
+            {lettersInput ? <p>Huh... we don't need letters here</p> : ''}
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-group col-6 mb-3">
+            <input
+              type="text"
+              value={input}
+              className="form-control"
+              placeholder="Enter the array"
+              onChange={this.handleChange}
+            />
+            <div className="input-group-append">
+              <button className="btn btn-outline-secondary" type="button"
+                      onClick={this.handleFilter}>Filter
+              </button>
             </div>
-        );
-    }
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <ul className="list-group">
+              {historyItems}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 export default App;
